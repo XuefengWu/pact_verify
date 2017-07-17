05c5b9a3a9e6389fcc5a44fb32594835ebd3ce4b
Xuefeng Wu <xfwu@thoughtworks.com>
2017-07-17 20:21:20 +0800

    ignore httpclient test

--- src/test/scala/com/thoughtworks/verify/pact/PactWSSpec.scala
+++ src/test/scala/com/thoughtworks/verify/pact/PactWSSpec.scala
@@ -7,12 +7,12 @@ import scala.util.Try
 class PactWSSpec extends FlatSpec with Matchers  {
 
 
-  "WS Client" should "visit web site " in {
+  /*"WS Client" should "visit web site " in {
     val ws = new PactWSImpl("https://passport.baidu.com")
     val request = PactRequest("get","/v2/sapi/authwidgetverify",None,None,None,None)
     val resultF: Try[HttpResponse] = ws.send(request)
     val status: Try[Int] = resultF.map(_.status)
     status should be (200)
-  }
+  }*/
 
 }
